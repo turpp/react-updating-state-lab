8f74e506056bee08e4d1a970a4eb45cacd882204
turpp <white.tripp@icloud.com>
2021-02-07 01:00:11 -0600

    commit tree 7214052bb578340a85d46f9607af672cf6736a53

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,25 @@
 // Code DigitalClicker Component Here
+import React from 'react'
+
+export default class DigitalClicker extends React.Component{
+    constructor(){
+        super()
+        this.state ={
+            timesClicked: 0
+        }
+    }
+
+    increase=()=>{
+       this.setState(previousState => {
+           return {timesClicked: previousState.timesClicked + 1}
+       })
+    }
+
+    render(){
+        return <button onClick={this.increase} type = 'button' >{`${this.state.timesClicked}`}</button>
+    }
+
+
+
+
+}
